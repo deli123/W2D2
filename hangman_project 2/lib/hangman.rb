bed0d15d1e25887f2c040ae0c46c554fcb30ce7e
Derek <liderek63@gmail.com>
2022-06-21 12:11:19 -0700

    part_2 done

--- hangman_project 2/lib/hangman.rb
+++ hangman_project 2/lib/hangman.rb
@@ -26,4 +26,49 @@ class Hangman
   def fill_indices(char, indices)
     indices.each { |i| @guess_word[i] = char}
   end
+
+  def try_guess(char)
+    if self.already_attempted?(char)
+      print "That has already been attemped"
+      return false
+    end
+
+    @attempted_chars << char
+    
+    if self.get_matching_indices(char).empty?
+      @remaining_incorrect_guesses -= 1
+    else
+      self.fill_indices(char, self.get_matching_indices(char))
+    end
+    return true
+  end
+
+  def ask_user_for_guess
+    print "Enter a char: "
+    self.try_guess(gets.chomp)
+  end
+
+  def win?
+    if @guess_word.join("") == @secret_word
+      print "WIN\n"
+      return true
+    end
+    return false
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      print "LOSE\n"
+      return true
+    end
+    return false
+  end
+
+  def game_over?
+    if self.win? || self.lose?
+      print @secret_word + "\n"
+      return true
+    end
+    return false
+  end
 end
