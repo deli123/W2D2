8f3f668adb95babda09f4b4c07e1729e45c9c5f4
Derek <liderek63@gmail.com>
2022-06-21 13:04:55 -0700

    completed room.rb

--- hotel_project/lib/room.rb
+++ hotel_project/lib/room.rb
@@ -1,3 +1,24 @@
 class Room
-  
+    attr_reader :capacity, :occupants
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
+
+    def full?
+        @occupants.length == @capacity
+    end
+
+    def available_space
+        @capacity - @occupants.length
+    end
+
+    def add_occupant(name)
+        if !self.full?
+            @occupants << name
+            true
+        else
+            false
+        end
+    end  
 end
