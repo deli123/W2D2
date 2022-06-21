73b9647a8c9188d741a3ba66027c86be433cb17a
Derek <liderek63@gmail.com>
2022-06-21 15:56:17 -0700

    completed

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -4,30 +4,59 @@ def convert_to_int(str)
 end
 
 # PHASE 3
+
+class CoffeeError < StandardError
+  def message
+    "Thanks for the coffee. Try again."
+  end
+end
+
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
+  elsif maybe_fruit == 'coffee'
+    raise CoffeeError
+  else
+    raise StandardError.new "I don't want this."
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError => e
+    puts e.message
+    retry
+  rescue StandardError => e
+    puts e.message
+  end
 end  
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    if name.length <= 0
+      raise ArgumentError.new "You didn't give a name"
+    end
+
+    if yrs_known < 5
+      raise ArgumentError.new "We haven't known each other long enough."
+    end
+
+    if fav_pastime.length <= 0
+      raise ArgumentError.new "You didn't tell me your favorite pastime"
+    end
+    
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
+
   end
 
   def talk_about_friendship
--- skeleton/user_script.rb
+++ skeleton/user_script.rb
@@ -1,10 +1,13 @@
 require_relative 'super_useful'
 
-puts "'five' == #{convert_to_int('five')}"
+puts "'five' == #{convert_to_int('5')}"
 
 feed_me_a_fruit
 
-sam = BestFriend.new('', 1, '')
+# sam = BestFriend.new('', 1, '')
+# sam = BestFriend.new('Bob', 1, '')
+# sam = BestFriend.new('Bob', 5, '')
+sam = BestFriend.new('Bob', 5, 'Fishing')
 
 sam.talk_about_friendship
 sam.do_friendstuff
